c5a5d84c038ee6c5697617b022cd5abf0f1841d4
Kishore Dubagunta <kishore.dubagunta@psolv.com>
2019-03-30 10:37:02 -0500

    added news slider

--- static/js/news.js
+++ static/js/news.js
@@ -1,4 +1,18 @@
 d3.json('/news').then(news => {
-  console.log(news)
-
+    console.log(news)
+    slide_container = d3.select('#carousel-inner')
+    slide_container.selectAll('div')
+       .data(news)
+       .enter()
+       .append("div")
+       .classed("carousel-item", true)
+       .html(newsitem => {
+           // console.log(newsitem)
+           return `<a href="${newsitem.href}"> \
+                        <img class="img-fluid d-block w-100"  src="${newsitem.image}"  width="100%">  </a> \
+                   <div> \
+                      <p> ${newsitem.title}</p> \
+                   </div>`
+       })
+     slide_container.select(".carousel-item").classed("active", true)
 });
